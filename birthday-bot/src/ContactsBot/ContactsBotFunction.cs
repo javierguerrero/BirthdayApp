924f5245b711a5ddd8a0631a1c8727fcc001dc50
Javier Guerrero <javier@bertonisolutions.com>
2023-04-28 22:55:43 -0500

    send sms

--- birthday-bot/src/ContactsBot/ContactsBotFunction.cs
+++ birthday-bot/src/ContactsBot/ContactsBotFunction.cs
@@ -1,16 +1,83 @@
-using System;
+using ContactsBot.Domain;
+using Infrastructure.Notification;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ContactsBot
 {
     public class ContactsBotFunction
     {
         [FunctionName("ContactsBotFunction")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var contacts = await GetTodayBirthdaysAsync();
+            foreach (var contact in contacts)
+            {
+                var result = await SendMessageToContactAsync(contact);
+            }
+
+            //https://damienaicheh.github.io/azure/azure-functions/dotnet/2022/05/10/use-settings-json-azure-function-en.html
+        }
+
+        public async Task<bool> SendMessageToContactAsync(Contact contact)
+        {
+            var result = false;
+            try
+            {
+                var message = $"¡Feliz cumple, {contact.Name}!. Te escribe Javier Guerrero para desearte un lindo día en compañia de familiares y amigos.";
+                INotifier notifier = new Notifier();
+                notifier.SendSms(contact.PhoneNumber, message);
+            }
+            catch (Exception ex)
+            {
+                //Logging
+                throw;
+            }
+
+            return result;
+        }
+
+        public async Task<ICollection<Contact>> GetTodayBirthdaysAsync()
+        {
+            ICollection<Contact> todayBirthdays = new List<Contact>();
+
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                var request = await httpClient.GetAsync("https://app-contactsapi-prod.azurewebsites.net/contacts");
+                request.EnsureSuccessStatusCode();
+
+                var contacts = JsonSerializer.Deserialize<List<Contact>>(
+                    await request.Content.ReadAsStringAsync(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    }
+                );
+
+                DateTime today = DateTime.Today;
+                foreach (var contact in contacts)
+                {
+                    //if (contact.Birthday.Day == today.Day && contact.Birthday.Month == today.Month)
+                    //{
+                        todayBirthdays.Add(contact);
+                    //}
+                }
+            }
+            catch (Exception ex)
+            {
+                // logging
+                throw;
+            }
+
+            return todayBirthdays;
         }
     }
 }
---
+++ birthday-bot/src/ContactsBot/Infrastructure/INotifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Notification
+{
+    public interface INotifier
+    {
+        public void SendSms(string toPhoneNumber, string message);
+    }
+}
